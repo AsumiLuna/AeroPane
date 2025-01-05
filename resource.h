2444c554d8f09cfbef058ffe0e654c44805179f3
AsumiLuna <162660668+AsumiLuna@users.noreply.github.com>
2025-01-05 02:19:22 -0600

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,11 @@
+#include "windows.h"
+
+
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+// Define resource IDs for the two icons
+#define IDI_AEROICON   101   // First icon ID (AeroIcon)
+#define IDI_BLANK      102   // Second icon ID (BlankIcon)
+
+#endif // RESOURCE_H
