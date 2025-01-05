821cd33eeecae8dbc8c1fa4e8bb73f74f29f9377
AsumiLuna <162660668+AsumiLuna@users.noreply.github.com>
2025-01-05 02:18:24 -0600

    Update aeropane.cpp
    Final update. See tags for more info.

--- aeropane.cpp
+++ aeropane.cpp
@@ -1,7 +1,8 @@
-#include <windows.h>
 #include <dwmapi.h>
 #include <iostream>
+#include <windows.h>
 #include "resource.h"
+
 
 #pragma comment(lib, "dwmapi.lib")
 
@@ -30,65 +31,63 @@ LRESULT CALLBACK SubWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
 
 // Create and init sub controls
 void CreateSubWindowControls(HWND hwnd) {
-    HWND hTextBox1, hTextBox2, hTextBox3, hTextBox4;
-    HWND hSpin1, hSpin2, hSpin3, hSpin4;
     HWND hLaunchButton;
     HWND hHideButton;
 
-    hTextBox1 = CreateWindowEx(
+    HWND hTextBox1 = CreateWindowEx(
         0, "EDIT", "", WS_CHILD | WS_VISIBLE | WS_BORDER,
-        10, 10, 100, 25, hwnd, (HMENU)ID_TEXT_BOX1,
-        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
+        10, 10, 100, 25, hwnd, (HMENU) ID_TEXT_BOX1,
+        (HINSTANCE) GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
     );
 
-    hTextBox2 = CreateWindowEx(
+    HWND hTextBox2 = CreateWindowEx(
         0, "EDIT", "", WS_CHILD | WS_VISIBLE | WS_BORDER,
-        10, 45, 100, 25, hwnd, (HMENU)ID_TEXT_BOX2,
-        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
+        10, 45, 100, 25, hwnd, (HMENU) ID_TEXT_BOX2,
+        (HINSTANCE) GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
     );
 
-    hTextBox3 = CreateWindowEx(
+    HWND hTextBox3 = CreateWindowEx(
         0, "EDIT", "", WS_CHILD | WS_VISIBLE | WS_BORDER,
-        10, 80, 100, 25, hwnd, (HMENU)ID_TEXT_BOX3,
-        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
+        10, 80, 100, 25, hwnd, (HMENU) ID_TEXT_BOX3,
+        (HINSTANCE) GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
     );
 
-    hTextBox4 = CreateWindowEx(
+    HWND hTextBox4 = CreateWindowEx(
         0, "EDIT", "", WS_CHILD | WS_VISIBLE | WS_BORDER,
-        10, 115, 100, 25, hwnd, (HMENU)ID_TEXT_BOX4,
-        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
+        10, 115, 100, 25, hwnd, (HMENU) ID_TEXT_BOX4,
+        (HINSTANCE) GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
     );
 
     // Create spin buttons
-    hSpin1 = CreateWindowEx(
+    HWND hSpin1 = CreateWindowEx(
         0, UPDOWN_CLASS, nullptr,
         WS_CHILD | WS_VISIBLE | UDS_ALIGNRIGHT | UDS_SETBUDDYINT,
-        115, 10, 20, 25, hwnd, (HMENU)ID_SPIN1,
-        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
+        115, 10, 20, 25, hwnd, (HMENU) ID_SPIN1,
+        (HINSTANCE) GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
     );
     SendMessage(hSpin1, UDM_SETRANGE, 0, MAKELPARAM(-1699999, 1700000)); // Set range -100 to 100
 
-    hSpin2 = CreateWindowEx(
+    HWND hSpin2 = CreateWindowEx(
         0, UPDOWN_CLASS, nullptr,
         WS_CHILD | WS_VISIBLE | UDS_ALIGNRIGHT | UDS_SETBUDDYINT,
-        115, 45, 20, 25, hwnd, (HMENU)ID_SPIN2,
-        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
+        115, 45, 20, 25, hwnd, (HMENU) ID_SPIN2,
+        (HINSTANCE) GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
     );
     SendMessage(hSpin2, UDM_SETRANGE, 0, MAKELPARAM(-1699999, 1700000)); // Set range -100 to 100
 
-    hSpin3 = CreateWindowEx(
+    HWND hSpin3 = CreateWindowEx(
         0, UPDOWN_CLASS, nullptr,
         WS_CHILD | WS_VISIBLE | UDS_ALIGNRIGHT | UDS_SETBUDDYINT,
-        115, 80, 20, 25, hwnd, (HMENU)ID_SPIN3,
-        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
+        115, 80, 20, 25, hwnd, (HMENU) ID_SPIN3,
+        (HINSTANCE) GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
     );
     SendMessage(hSpin3, UDM_SETRANGE, 0, MAKELPARAM(-1699999, 1700000)); // Set range -100 to 100
 
-    hSpin4 = CreateWindowEx(
+    HWND hSpin4 = CreateWindowEx(
         0, UPDOWN_CLASS, nullptr,
         WS_CHILD | WS_VISIBLE | UDS_ALIGNRIGHT | UDS_SETBUDDYINT,
-        115, 115, 20, 25, hwnd, (HMENU)ID_SPIN4,
-        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
+        115, 115, 20, 25, hwnd, (HMENU) ID_SPIN4,
+        (HINSTANCE) GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
     );
     SendMessage(hSpin4, UDM_SETRANGE, 0, MAKELPARAM(-1699999, 1700000)); // Set range -100 to 100
 
@@ -97,13 +96,19 @@ void CreateSubWindowControls(HWND hwnd) {
     SendMessage(hSpin3, UDM_SETBUDDY, (WPARAM)hTextBox3, 0);
     SendMessage(hSpin4, UDM_SETBUDDY, (WPARAM)hTextBox4, 0);
 
+    auto hFont = static_cast<HFONT>(GetStockObject(DEFAULT_GUI_FONT));
+
     hLaunchButton = CreateWindow(
         "BUTTON", "Create Window",
         WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
         10, 150, 150, 30,
         hwnd, (HMENU)ID_BUTTON_LAUNCH_MAIN,
         (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr
+
     );
+
+    SendMessage(hLaunchButton, WM_SETFONT, (WPARAM)hFont, TRUE);
+
 }
 
 // Function to create the main window and handle messages
@@ -145,9 +150,64 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 LRESULT CALLBACK SubWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
 
-        case WM_CREATE:
+        case WM_CREATE: {
             CreateSubWindowControls(hwnd);
+
+            // Create a system font
+            HFONT hFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
+
+            // Create the labels (STATIC controls) with the default background (transparent)
+            HWND hLabel1 = CreateWindow("STATIC", "Top frame:", WS_VISIBLE | WS_CHILD, 10, 10, 50, 25, hwnd, nullptr, nullptr, nullptr);
+            SendMessage(hLabel1, WM_SETFONT, (WPARAM)hFont, TRUE);
+
+            HWND hLabel2 = CreateWindow("STATIC", "Bottom frame:", WS_VISIBLE | WS_CHILD, 10, 45, 50, 25, hwnd, nullptr, nullptr, nullptr);
+            SendMessage(hLabel2, WM_SETFONT, (WPARAM)hFont, TRUE);
+
+            HWND hLabel3 = CreateWindow("STATIC", "Left frame:", WS_VISIBLE | WS_CHILD, 10, 80, 49, 25, hwnd, nullptr, nullptr, nullptr);
+            SendMessage(hLabel3, WM_SETFONT, (WPARAM)hFont, TRUE);
+
+            HWND hLabel4 = CreateWindow("STATIC", "Right frame:", WS_VISIBLE | WS_CHILD, 10, 115, 50, 25, hwnd, nullptr, nullptr, nullptr);
+            SendMessage(hLabel4, WM_SETFONT, (WPARAM)hFont, TRUE);
+
+            // Add label below all boxes
+            HWND hInstructionLabel = CreateWindow("STATIC",
+                "To create a full glass window, input \"-1\" into any of the 4 margin boxes.",
+                WS_VISIBLE | WS_CHILD, 10, 150, 400, 35, hwnd, nullptr, nullptr, nullptr);
+            SendMessage(hInstructionLabel, WM_SETFONT, (WPARAM)hFont, TRUE);
+
             break;
+        }
+
+        case WM_CTLCOLORSTATIC: {
+            HDC hdcStatic = (HDC)wParam;
+            HWND hwndStatic = (HWND)lParam;
+
+            // Set the background color of the STATIC control to white
+            SetBkColor(hdcStatic, RGB(255, 255, 255));
+
+            // Create a white brush for the background
+            HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 255));
+
+            // Set the brush for the STATIC control
+            SetBkMode(hdcStatic, OPAQUE);
+            SetTextColor(hdcStatic, RGB(0, 0, 0));  // Set text color to black
+
+            return (LRESULT)hBrush;
+        }
+
+
+
+
+        case WM_GETMINMAXINFO: {
+            auto* pMinMaxInfo = reinterpret_cast<MINMAXINFO *>(lParam);
+
+            // Set minimum window size
+            pMinMaxInfo->ptMinTrackSize.x = 450; // Minimum width
+            pMinMaxInfo->ptMinTrackSize.y = 300; // Minimum height
+
+            return 0;
+        }
+
 
         case WM_COMMAND: {
             if (LOWORD(wParam) == ID_BUTTON_LAUNCH_MAIN) {
@@ -162,7 +222,6 @@ LRESULT CALLBACK SubWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
                 GetWindowText(GetDlgItem(hwnd, ID_TEXT_BOX4), buffer, sizeof(buffer));
                 margin4 = atoi(buffer);
 
-
                 HWND hMainWindow = CreateWindowEx(
                     WS_EX_COMPOSITED | WS_EX_LAYERED,
                     ID_MAIN_WINDOW_CLASS, nullptr,
@@ -170,8 +229,6 @@ LRESULT CALLBACK SubWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
                     CW_USEDEFAULT, CW_USEDEFAULT, 14000, 14000,
                     nullptr, nullptr, reinterpret_cast<HINSTANCE>(GetWindowLongPtr(hwnd, GWLP_HINSTANCE)), nullptr
                 );
-
-
 
                 if (hMainWindow != nullptr) {
                     MARGINS margins = { margin1, margin2, margin3, margin4 };
@@ -190,14 +247,14 @@ LRESULT CALLBACK SubWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
             int width = rcClient.right;
             int height = rcClient.bottom;
 
-            SetWindowPos(GetDlgItem(hwnd, ID_TEXT_BOX1), nullptr, 22, 10, width - 45, 25, SWP_NOZORDER | SWP_NOACTIVATE);
-            SetWindowPos(GetDlgItem(hwnd, ID_TEXT_BOX2), nullptr, 22, 45, width - 45, 25, SWP_NOZORDER | SWP_NOACTIVATE);
-            SetWindowPos(GetDlgItem(hwnd, ID_TEXT_BOX3), nullptr, 22, 80, width - 45, 25, SWP_NOZORDER | SWP_NOACTIVATE);
-            SetWindowPos(GetDlgItem(hwnd, ID_TEXT_BOX4), nullptr, 22, 115, width - 45, 25, SWP_NOZORDER | SWP_NOACTIVATE);
-            SetWindowPos(GetDlgItem(hwnd, ID_SPIN1), nullptr, width - 22, 10, 20, 25, SWP_NOZORDER | SWP_NOACTIVATE);
-            SetWindowPos(GetDlgItem(hwnd, ID_SPIN2), nullptr, width - 22, 45, 20, 25, SWP_NOZORDER | SWP_NOACTIVATE);
-            SetWindowPos(GetDlgItem(hwnd, ID_SPIN3), nullptr, width - 22, 80, 20, 25, SWP_NOZORDER | SWP_NOACTIVATE);
-            SetWindowPos(GetDlgItem(hwnd, ID_SPIN4), nullptr, width - 22, 115, 20, 25, SWP_NOZORDER | SWP_NOACTIVATE);
+            SetWindowPos(GetDlgItem(hwnd, ID_TEXT_BOX3), nullptr, 60, 10, width - 85, 25, SWP_NOZORDER | SWP_NOACTIVATE);
+            SetWindowPos(GetDlgItem(hwnd, ID_TEXT_BOX4), nullptr, 60, 45, width - 85, 25, SWP_NOZORDER | SWP_NOACTIVATE);
+            SetWindowPos(GetDlgItem(hwnd, ID_TEXT_BOX1), nullptr, 60, 80, width - 85, 25, SWP_NOZORDER | SWP_NOACTIVATE);
+            SetWindowPos(GetDlgItem(hwnd, ID_TEXT_BOX2), nullptr, 60, 115, width - 85, 25, SWP_NOZORDER | SWP_NOACTIVATE);
+            SetWindowPos(GetDlgItem(hwnd, ID_SPIN3), nullptr, width - 22, 10, 20, 25, SWP_NOZORDER | SWP_NOACTIVATE);
+            SetWindowPos(GetDlgItem(hwnd, ID_SPIN4), nullptr, width - 22, 45, 20, 25, SWP_NOZORDER | SWP_NOACTIVATE);
+            SetWindowPos(GetDlgItem(hwnd, ID_SPIN1), nullptr, width - 22, 80, 20, 25, SWP_NOZORDER | SWP_NOACTIVATE);
+            SetWindowPos(GetDlgItem(hwnd, ID_SPIN2), nullptr, width - 22, 115, 20, 25, SWP_NOZORDER | SWP_NOACTIVATE);
 
             SetWindowPos(GetDlgItem(hwnd, ID_BUTTON_LAUNCH_MAIN), nullptr, 10, height - 45, width - 20, 30, SWP_NOZORDER | SWP_NOACTIVATE);
             break;
@@ -211,6 +268,7 @@ LRESULT CALLBACK SubWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
     }
     return 0;
 }
+
 
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -231,7 +289,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
         WS_EX_APPWINDOW,
         ID_SUB_WINDOW_CLASS, "Configure Margins",
         WS_OVERLAPPEDWINDOW,
-        800, 600, 300, 250,
+        800, 600, 450, 300,
         nullptr, nullptr, hInstance, nullptr
     );
 
